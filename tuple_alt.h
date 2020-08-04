5c8b4e0a982a7ed727e9998a23e012e84699e906
Piotr Stolarz <pstolarz@o2.pl>
2020-08-04 19:48:22 +0200

    tuple_alt refactored

--- tuple_alt.h
+++ tuple_alt.h
@@ -1,111 +1,128 @@
 #ifndef __TUPLE_ALT_H__
 #define __TUPLE_ALT_H__
 
-#include <utility>  // std::integer_sequence
+#include <type_traits>
+#include <utility>  // C++14 std::integer_sequence
 
 namespace tuple_alt {
 
-template<size_t index, class Head, class ...Tail>
-struct _Tuple: _Tuple<index+1, Tail...>
+template<std::size_t Index, typename Head, typename ...Tail>
+struct _Tuple: _Tuple<Index+1, Tail...>
 {
-    typedef typename std::remove_reference<Head>::type elem_type;
-    Head&& h;
+    Head&& h; // Head lvalue with possible references to Head rvalues
 
-    typedef _Tuple<index+1, Tail...> base_type;
-
-    constexpr _Tuple(Head&& h, Tail&&... tail):
-        base_type(std::forward<decltype(tail)>(tail)...),
-        h(std::forward<decltype(h)>(h)) {}
+    _Tuple(Head&& h, Tail&&... tail):
+        _Tuple<Index+1, Tail...>(std::forward<decltype(tail)>(tail)...),
+        /*
+         * NOTES:
+         * 1. 'h(h)' doesn't work here since 'h' (as function argument) is
+         * treated as Head lvalue and '_Tuple::h' is considered as Head rvalue,
+         * therefore there is a need to forward 'h' or cast 'h' as:
+         * 'h(static_cast<decltype(h)>(h))' which is equivalent to forwarding.
+         *
+         * 2. 'h(std::move(h))' also doesn't work here since 'std::move(h)' is
+         * Head rvalue and '_Tuple::h' is considered in this case as Head lvalue.
+         *
+         * As a conclusion T&& may be considered as rvalue/lvalue reference
+         * (depending on context) therefore any assignments between values of
+         * such types requires explicit casting (type forwarding).
+         */
+        h(std::forward<decltype(h)>(h))
+    {}
 
     Head&& get_elem() const {
-        return h;
-    }
-
-    base_type& get_base() const {
-        return static_cast<base_type&>(*this);
-    }
-
-    void *get_elem_addr(size_t n) const
-    {
-        return (n == index ?
-            const_cast<void*>((const void*)&h) :
-            base_type::get_elem_addr(n));
+        return std::forward<decltype(h)>(h);
     }
 };
 
-template<size_t index, class Head>
-struct _Tuple<index, Head>
+template<std::size_t Index, typename Head>
+struct _Tuple<Index, Head>
 {
-    typedef typename std::remove_reference<Head>::type elem_type;
     Head&& h;
 
-    constexpr _Tuple(Head&& h): h(std::forward<decltype(h)>(h)) {}
+    _Tuple(Head&& h):
+        h(std::forward<decltype(h)>(h))
+    {}
 
     Head&& get_elem() const {
-        return h;
+        return std::forward<decltype(h)>(h);
     }
 
-    void *get_elem_addr(size_t n) const
-    {
-        return (n == index ?
-            const_cast<void*>((const void*)&h) :
-            nullptr);
-    }
-
-    static constexpr size_t size = index+1;
+    static constexpr std::size_t size = Index+1;
 };
 
-template<class ...List>
-using Tuple = _Tuple<0, List...>;
-
-template<class ...List>
-Tuple<List...> constexpr tuple_alt(List&&... list)
+template<typename ...List>
+_Tuple<0, List...> tuple_alt(List&&... list)
 {
-    return Tuple<List...>(std::forward<decltype(list)>(list)...);
+    return _Tuple<0, List...>(std::forward<decltype(list)>(list)...);
 };
 
 
-template<size_t index, class Tuple>
-struct TupleTrait: TupleTrait<index-1, typename Tuple::base_type>
+template<std::size_t N, bool RecurCond, typename Tuple>
+struct _get_tuple {};
+
+template<std::size_t N, std::size_t Index, typename Head, typename ...Tail>
+struct _get_tuple<N, false, _Tuple<Index, Head, Tail...>>:
+    _get_tuple<N-1, !(N-1), _Tuple<Index+1, Tail...>>
 {};
 
-template<class Tuple>
-struct TupleTrait<0, Tuple>
+template<std::size_t Index, typename ...Types>
+struct _get_tuple<0, true, _Tuple<Index, Types...>>
 {
-    typedef typename std::remove_reference<Tuple>::type tuple_type;
-    typedef typename Tuple::elem_type elem_type;
+    using type = _Tuple<Index, Types...>;
 };
 
-template<size_t n, class Tuple>
-auto&& get_elem(Tuple& t)
+template<std::size_t N, std::size_t Index, typename ...Types>
+using get_tuple = _get_tuple<N, !N, _Tuple<Index, Types...>>;
+
+template<std::size_t N, std::size_t Index, typename ...Types>
+auto get_elem(const _Tuple<Index, Types...>& t)
+    // C++11 required
+    // -> decltype(std::declval<
+    //    typename get_tuple<N, Index, Types...>::type>().get_elem())
 {
-    static_assert(n >= 0 && n < Tuple::size, "Invalid depth");
-    return *reinterpret_cast<typename TupleTrait<n, Tuple>::elem_type*>(
-        t.get_elem_addr(n));
+    static_assert(N >= 0 && N < _Tuple<Index, Types...>::size, "Invalid depth");
+    return static_cast<
+        const typename get_tuple<N, Index, Types...>::type*>(&t)->get_elem();
 }
 
-template<class Seq>
+
+template<typename Seq>
 struct _print_elems;
 
 template<std::size_t ...Indexes>
 struct _print_elems<std::integer_sequence<std::size_t, Indexes...>>
 {
-    template<class Tuple>
-    static void print(Tuple& t) {
+    template<std::size_t Index, typename ...Types>
+    static void print(const _Tuple<Index, Types...>& t) {
+        // use initializer-list to ensure proper elements order
         auto l = {(std::cout << get_elem<Indexes>(t) << "\n", 0)...};
     }
 };
 
-template<size_t N, class Tuple>
-void print_elems(Tuple& t) {
-    _print_elems<std::make_index_sequence<N>>::print(t);
+template<std::size_t Index, typename ...Types>
+void print_elems(const _Tuple<Index, Types...>& t) {
+    _print_elems<std::make_index_sequence<sizeof...(Types)>>::print(t);
 }
+
 
 /*
  * test
  */
-static void _f1(void) {}
-static int _f2(int) { return 0; }
+static void f() {}
+struct A {};
+
+std::ostream& operator<<(std::ostream& os, const A& a)
+{
+    std::cout << "struct A lvalue";
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const A&& a)
+{
+    std::cout << "struct A rvalue";
+    return os;
+}
 
 void test()
 {
@@ -114,8 +131,12 @@ void test()
     char str[] = "lv-string";
     int *pi = &i;
 
-    auto t = tuple_alt(1, 2.5, "rv-string", i, d, str, pi, _f1, _f2);
-    print_elems<9>(t);
+    A a;
+    auto t = tuple_alt(1, 2.5, "rv-string", i, d, str, pi, f, a, A{});
+    print_elems(t);
+
+    auto e = get_elem<2>(t);
+    std::cout << "Tuple element #2: " << e << "\n";
 }
 
 } // namespace tuple_alt
